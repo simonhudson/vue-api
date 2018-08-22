6a78e88a1440c3343d13cc883e0928d234cd6e8d
Simon Hudson <hellosimonhudson+github@gmail.com>
2018-08-22 12:26:16 +0100

    multiple instances

--- js/main.js
+++ js/main.js
@@ -2,8 +2,32 @@
 
 const apiUrl = 'https://swapi.co/api/';
 
-const app = new Vue({
-	el: '#app',
+const getData = (instance, e) => {
+	instance.isLoading = true;
+	const endpoint = e.target.dataset.endpoint;
+	instance.loadingMessage = `Loading...`;
+	fetch(`${apiUrl}${endpoint}`)
+		.then(response => response.json())
+		.then(data => handleResponse(instance, data, endpoint))
+		.catch(error => handleError(instance, error, endpoint));
+};
+
+const handleResponse = (instance, data, endpoint) => {
+	instance.apiData[endpoint] = data;
+	console.log('--------------------');
+	console.log(instance.apiData);
+	console.log('--------------------');
+	instance.isLoading = false;
+};
+
+const handleError = (instance, error, endpoint) => {
+	instance.isLoading = false;
+	instance.hasError = true;
+	instance.errorMessage = `Sorry, there's been a problem getting /${endpoint}`;
+};
+
+const films = new Vue({
+	el: '#films',
 	data: {
 		apiData: {},
 		isLoading: false,
@@ -13,29 +37,42 @@ const app = new Vue({
 	},
 	methods: {
 		getData(e) {
-			this.isLoading = true;
-			const endpoint = e.target.dataset.endpoint;
-			this.loadingMessage = `Loading /${endpoint}`;
-			fetchData(endpoint);
+			getData(this, e);
 		}
 	}
 });
 
-const handleResponse = (data, endpoint) => {
-	app.apiData[endpoint] = data;
-	app.isLoading = false;
-};
+const people = new Vue({
+	el: '#people',
+	data: {
+		apiData: {},
+		isLoading: false,
+		loadingMessage: '',
+		hasError: true,
+		errorMessage: ''
+	},
+	methods: {
+		getData(e) {
+			getData(this, e);
+		}
+	}
+});
 
-const handleError = (error, endpoint) => {
-	app.isLoading = false;
-	app.hasError = true;
-	app.errorMessage = `Sorry, there's been a problem getting /${endpoint}`;
-};
-
-const fetchData = endpoint => {
-	
-	fetch(`${apiUrl}${endpoint}`)
-		.then(response => response.json())
-		.then(data => handleResponse(data, endpoint))
-		.catch(error => handleError(error, endpoint));
-};
+// const handleResponse = (instance, data, endpoint) => {
+// 	instance.apiData[endpoint] = data;
+// 	instance.isLoading = false;
+// };
+// 
+// const handleError = (error, endpoint) => {
+// 	instance.isLoading = false;
+// 	instance.hasError = true;
+// 	instance.errorMessage = `Sorry, there's been a problem getting /${endpoint}`;
+// };
+// 
+// const fetchData = endpoint => {
+// 
+// 	fetch(`${apiUrl}${endpoint}`)
+// 		.then(response => response.json())
+// 		.then(data => handleResponse(data, endpoint))
+// 		.catch(error => handleError(error, endpoint));
+// };

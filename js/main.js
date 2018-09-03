c9178068d7739bba1f6c9fd36cf352f60bce9f37
Simon Hudson <hellosimonhudson+github@gmail.com>
2018-09-03 08:05:47 +0100

    add sort

--- js/main.js
+++ js/main.js
@@ -2,21 +2,40 @@
 
 const API_URL = 'https://swapi.co/api/';
 
+const sortData = (data, sortKey) => {
+	if (
+		!data ||
+		!sortKey ||
+		!Array.isArray(data) ||
+		!data.length ||
+		!(data[0] instanceof Object) ||
+		typeof sortKey !== 'string'
+	)
+		return data;
+
+	const sorted = data.sort((a, b) => {
+		return ~~(a[sortKey] > b[sortKey]);
+	});
+	return sorted;
+};
+
 const initVue = element => {
 	const endpoint = element.dataset.endpoint;
 	return {
 		el: `[data-endpoint=${endpoint}]`,
 		data: {
-			endpoint,
 			apiData: {},
-			isLoading: false,
-			loadingMessage: '',
+			currentSortKey: '',
+			endpoint,
+			errorMessage: '',
 			hasError: true,
-			errorMessage: ''
+			isLoading: false,
+			loadingMessage: ''
 		},
 		methods: {
 			getData(e) {
 				this.isLoading = true;
+				this.apiData[endpoint] = null;
 				this.loadingMessage = `Loading /${endpoint}`;
 				fetch(`${API_URL}${endpoint}`)
 					.then(response => response.json())
@@ -29,6 +48,10 @@ const initVue = element => {
 						this.hasError = true;
 						this.errorMessage = `Sorry, there's been a problem getting /${endpoint}`;
 					});
+			},
+			sort(e) {
+				this.currentSortKey = e.target.selectedOptions[0].value;
+				this.apiData[endpoint].results = sortData(this.apiData[endpoint].results, this.currentSortKey);
 			}
 		}
 	}
